89c9d02f37685452b2923bed55a8bb48b941c13a
kashyapSheladiya <kashyap27041993@gmail.com>
2021-08-18 18:04:25 +0200

    implementation event sync in comment-moderation

--- blog/comment-moderation/index.js
+++ blog/comment-moderation/index.js
@@ -5,9 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = async (type, data) => {
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
@@ -21,10 +19,23 @@ app.post('/events', async (req, res) => {
       }
     });
   };
+};
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
   console.log('Listening on 4003');
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event from moderation: ', event.type);
+    if (event.type === 'CommentCreated') {
+      handleEvent(event.type, event.data);
+    }
+  }
 });
