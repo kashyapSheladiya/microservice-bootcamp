97893afc730109ec7a4b6ca00101444c1e092b8f
kashyapSheladiya <kashyap27041993@gmail.com>
2021-08-18 17:25:16 +0200

    rendering comments based on status

--- blog/client/src/CommentsList.js
+++ blog/client/src/CommentsList.js
@@ -3,7 +3,19 @@ import React from 'react';
 function CommentsList({ comments }) {
 
   const renderComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>
+    let content;
+    switch(comment.status) {
+      case 'rejected':
+        content = "Comment is rejected.";
+        break;
+      case 'pending':
+        content = "Comment is under moderation.";
+        break;
+      default:
+        content = comment.content;
+    }
+
+    return <li key={comment.id}>{content}</li>
   });
   return(
     <ul>{renderComments}</ul>
