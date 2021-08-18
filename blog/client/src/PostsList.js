024fa9b70810886c713be247e6f922ac8c45e728
kashyapSheladiya <kashyap27041993@gmail.com>
2021-08-18 10:23:42 +0200

    displaying comments

--- blog/client/src/PostsList.js
+++ blog/client/src/PostsList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentsList from './CommentsList';
 
 function PostsList() {
   const [posts, setPosts] = useState({});
@@ -22,6 +23,7 @@ function PostsList() {
         <div className="card" style={{ width: '30%', marginBottom: '20px'}} key={post.id}>
             <div className="card-body">
               <h3>{post.title}</h3>
+              <CommentsList postId={post.id}/>
               <CommentCreate postId={post.id}/>
             </div>
         </div>
